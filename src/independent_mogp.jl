16b8eee2f2f15506415dd817f2c36b987b3ffc29
WT <wt0881@my.bristol.ac.uk>
2021-09-24 15:44:05 +0100

    Extend independent mogp to by-features

--- src/independent_mogp.jl
+++ src/independent_mogp.jl
@@ -28,9 +28,7 @@ julia> ind_mogp1.fs == ind_mogp2.fs
 true
 ```
 """
-function independent_mogp(fs::Vector{<:AbstractGP})
-    return IndependentMOGP(fs)
-end
+independent_mogp(fs::Vector{<:AbstractGP}) = IndependentMOGP(fs)
 
 """
     finite_gps(fx)
@@ -42,11 +40,11 @@ function finite_gps(fx::FiniteGP{<:IndependentMOGP, <:MOInputIsotopicByOutputs},
     return [f(fx.x.x, σ²) for f in fx.f.fs]
 end
 
-const IsotropicFiniteIndependentMOGP = FiniteGP{
+const IsotropicByOutputsFiniteIndependentMOGP = FiniteGP{
     <:IndependentMOGP, <:MOInputIsotopicByOutputs, <:Diagonal{<:Real, <:Fill},
 }
 
-# Implement AbstractGPs API
+# Optimisations for MOInputIsotopicByOutputs.
 
 # See AbstractGPs.jl API docs.
 function AbstractGPs.mean(f::IndependentMOGP, x::MOInputIsotopicByOutputs)
@@ -73,14 +71,14 @@ function AbstractGPs.cov(
 end
 
 # See AbstractGPs.jl API docs.
-function AbstractGPs.logpdf(ft::IsotropicFiniteIndependentMOGP, y::AbstractVector)
+function AbstractGPs.logpdf(ft::IsotropicByOutputsFiniteIndependentMOGP, y::AbstractVector)
     finiteGPs = finite_gps(ft, ft.Σy[1])
     ys = collect(eachcol(reshape(y, (length(ft.x.x), :))))
     return sum(map(logpdf, finiteGPs, ys))
 end
 
 # See AbstractGPs.jl API docs.
-function AbstractGPs.rand(rng::AbstractRNG, ft::IsotropicFiniteIndependentMOGP)
+function AbstractGPs.rand(rng::AbstractRNG, ft::IsotropicByOutputsFiniteIndependentMOGP)
     finiteGPs = finite_gps(ft, ft.Σy[1])
     return reduce(vcat, map(fx -> rand(rng, fx), finiteGPs))
 end
@@ -89,7 +87,9 @@ end
 AbstractGPs.rand(ft::FiniteGP{<:IndependentMOGP}) = rand(Random.GLOBAL_RNG, ft)
 
 # See AbstractGPs.jl API docs.
-function AbstractGPs.rand(rng::AbstractRNG, ft::IsotropicFiniteIndependentMOGP, N::Int)
+function AbstractGPs.rand(
+    rng::AbstractRNG, ft::IsotropicByOutputsFiniteIndependentMOGP, N::Int,
+)
     return reduce(hcat, [rand(rng, ft) for _ in 1:N])
 end
 
@@ -115,10 +115,69 @@ Posterior implementation for isotopic inputs, given diagonal Σy (OILMM).
 See AbstractGPs.jl API docs.
 """
 function AbstractGPs.posterior(
-    ft::IsotropicFiniteIndependentMOGP, y::AbstractVector{<:Real}
+    ft::IsotropicByOutputsFiniteIndependentMOGP, y::AbstractVector{<:Real}
 )
     finiteGPs = finite_gps(ft, ft.Σy[1])
     ys = collect(eachcol(reshape(y, (length(ft.x.x), :))))
     ind_posts = [AbstractGPs.posterior(fx, y_i) for (fx, y_i) in zip(finiteGPs, ys)]
     return independent_mogp(ind_posts)
+end
+
+
+# AbstractGPs APIs implementations for MOInputIsotopicByFeatures.
+
+const IsotropicByFeaturesFiniteIndependentMOGP = FiniteGP{
+    <:IndependentMOGP, <:MOInputIsotopicByFeatures, <:Diagonal{<:Real, <:Fill},
+}
+
+function reorder_features_to_outputs_indices(x::MOInputIsotopicByOutputs)
+    return sortperm(vec(reshape(1:length(x), x.out_dim, length(x.x))'))
+end
+
+function reorder_features_to_outputs_indices(x::MOInputIsotopicByFeatures)
+    return vec(reshape(1:length(x), x.out_dim, length(x.x))')
+end
+
+reorder_by_outputs(x::MOInputIsotopicByFeatures) = MOInputIsotopicByOutputs(x.x, x.out_dim)
+
+# We know that the observation noise covariance is constant-diagonal, so no need to reorder.
+function reorder_by_outputs(fx::IsotropicByFeaturesFiniteIndependentMOGP)
+    return FiniteGP(fx.f, reorder_by_outputs(fx.x), fx.Σy)
+end
+
+ChainRulesCore.@non_differentiable reorder_features_to_outputs_indices(::Any)
+ChainRulesCore.@non_differentiable reorder_by_outputs(::Any)
+
+function finite_gps(fx::FiniteGP{<:IndependentMOGP, <:MOInputIsotopicByFeatures}, σ²::Real)
+    return [f(fx.x.x, σ²) for f in fx.f.fs]
+end
+
+function AbstractGPs.mean(f::IndependentMOGP, x::MOInputIsotopicByFeatures)
+    x_by_outputs = reorder_by_outputs(x)
+    mean_by_outputs = mean(f, x_by_outputs)
+    return mean_by_outputs[reorder_features_to_outputs_indices(x_by_outputs)]
+end
+
+function AbstractGPs.var(f::IndependentMOGP, x::MOInputIsotopicByFeatures)
+    x_by_outputs = reorder_by_outputs(x)
+    var_by_outputs = var(f, x_by_outputs)
+    return var_by_outputs[reorder_features_to_outputs_indices(x_by_outputs)]
+end
+
+function AbstractGPs.cov(f::IndependentMOGP, x::MOInputIsotopicByFeatures)
+    x_by_outputs = reorder_by_outputs(x)
+    C_by_outputs = cov(f, x_by_outputs)
+    idx = reorder_features_to_outputs_indices(x_by_outputs)
+    return C_by_outputs[idx, idx]
+end
+
+function AbstractGPs.rand(rng::AbstractRNG, ft::IsotropicByFeaturesFiniteIndependentMOGP)
+    finiteGPs = finite_gps(ft, ft.Σy[1])
+    return vec(reduce(hcat, map(fx -> rand(rng, fx), finiteGPs))')
+end
+
+function AbstractGPs.logpdf(
+    ft::IsotropicByFeaturesFiniteIndependentMOGP, y::AbstractVector{<:Real},
+)
+    return logpdf(reorder_by_outputs(ft), y[reorder_features_to_outputs_indices(ft.x)])
 end
