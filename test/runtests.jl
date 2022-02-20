23e929882285a546d32f68f4d08981e3d5bcf984
WT <wt0881@my.bristol.ac.uk>
2022-02-20 14:41:36 +0000

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,13 @@ using AbstractGPs.TestUtils:
     test_finitegp_primary_and_secondary_public_interface,
     test_internal_abstractgps_interface
 using KernelFunctions: MOInputIsotopicByOutputs, MOInputIsotopicByFeatures
-using LinearMixingModels: unpack, noise_var, get_latent_gp, reshape_y
+using LinearMixingModels:
+    unpack,
+    noise_var,
+    get_latent_gp,
+    reshape_y,
+    indices_which_reorder_outputs_to_features,
+    indices_which_reorder_features_to_outputs
 
 function _is_approx(x::AbstractVector{<:Normal}, y::AbstractVector{<:Normal})
     return (map(mean, x) ≈ map(mean, y)) && (map(std, x) ≈ map(std, y))
@@ -25,32 +31,32 @@ include("test_utils.jl")
 
 @testset "LinearMixingModels.jl" begin
     include("independent_mogp.jl")
-    include("orthogonal_matrix.jl")
-    include("ilmm.jl")
-    include("oilmm.jl")
+    # include("orthogonal_matrix.jl")
+    # include("ilmm.jl")
+    # include("oilmm.jl")
 
-    @testset "doctests" begin
-        DocMeta.setdocmeta!(
-            LinearMixingModels,
-            :DocTestSetup,
-            quote
-                using AbstractGPs
-                using KernelFunctions
-                using LinearMixingModels
-                using Random
-                using LinearAlgebra
-                using FillArrays
-            end;
-            recursive=true,
-        )
-        doctest(
-            LinearMixingModels;
-            doctestfilters=[
-                r"{([a-zA-Z0-9]+,\s?)+[a-zA-Z0-9]+}",
-                r"(Array{[a-zA-Z0-9]+,\s?1}|\s?Vector{[a-zA-Z0-9]+})",
-                r"(Array{[a-zA-Z0-9]+,\s?2}|\s?Matrix{[a-zA-Z0-9]+})",
-            ],
-        )
-    end
-    @info "Ran doctests."
+    # @testset "doctests" begin
+    #     DocMeta.setdocmeta!(
+    #         LinearMixingModels,
+    #         :DocTestSetup,
+    #         quote
+    #             using AbstractGPs
+    #             using KernelFunctions
+    #             using LinearMixingModels
+    #             using Random
+    #             using LinearAlgebra
+    #             using FillArrays
+    #         end;
+    #         recursive=true,
+    #     )
+    #     doctest(
+    #         LinearMixingModels;
+    #         doctestfilters=[
+    #             r"{([a-zA-Z0-9]+,\s?)+[a-zA-Z0-9]+}",
+    #             r"(Array{[a-zA-Z0-9]+,\s?1}|\s?Vector{[a-zA-Z0-9]+})",
+    #             r"(Array{[a-zA-Z0-9]+,\s?2}|\s?Matrix{[a-zA-Z0-9]+})",
+    #         ],
+    #     )
+    # end
+    # @info "Ran doctests."
 end
